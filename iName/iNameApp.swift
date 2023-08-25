35c470c6b5c3453702c711481238671f98a8a396
Oleksii Yakymchuk <oles0305@gmail.com>
2023-08-25 16:42:34 +0300

    Initial Commit

---
+++ iName/iNameApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNameApp.swift
+//  iName
+//
+//  Created by Олексій Якимчук on 25.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct iNameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
